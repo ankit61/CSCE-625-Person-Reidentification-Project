a33fa040a22d22e8fe34ddccc8e3162fa5c5dcba
ankit61 <ankit61@tamu.edu>
2018-10-23 19:28:51 -0500

    adding modified conv layer and renaming mnist script

---
+++ RealConv.py
@@ -0,0 +1,28 @@
+import torch
+from math import floor
+
+class RealConv2D(torch.nn.Module):
+    def __init__(self, in_channels, out_channels, kernel_size, stride=1, padding=0, dilation=1, groups=1, bias=True):
+        super(RealConv2D, self).__init__()
+        self.conv = conv2d(1, out_channels, kernel_size, stride, padding, dilation, group, bias)
+        
+        self.kernelSize = kernel_size
+        if type(kernel_size) is not tuple:
+            self.kernelSize = (kernel_size, kernel_size)
+
+        self.stride = stride
+        self.padding = padding
+        self.dilation = dilation
+        self.outChannels = out_channels
+
+    def forward(self, x):
+        temp = torch.tensor.empty(x.size(1), x.size(0), self.outChannels, getOutputHeight(x.size(2)), getOutputWidth(x.size(3)))
+        for i in range(0, x.size(1)): #loop through number of channels
+            temp[i, :, :, :, :] = self.conv(x[:, i, :, :]) #FIXME: copy from GPU to CPU?
+        return torch.max(temp, 0)
+        
+    def getOutputHeight(h):
+        return floor(((h + (2 * self.padding) - (self.dilation * (self.kernelSize[0] - 1)) - 1) / self.stride) + 1)
+
+    def getOutputWidth(w):
+        return floor(((w + (2 * self.padding) - (self.dilation * (self.kernelSize[1] - 1)) - 1) / self.stride) + 1)
