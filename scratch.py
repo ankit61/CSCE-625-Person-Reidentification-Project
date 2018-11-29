67af2873ba40216f12145669eb435ded91055c95
Benton Guess <bguess10@tamu.edu>
2018-11-29 17:04:10 -0600

    Bug fixes to mAP

--- scratch.py
+++
@@ -1,2 +0,0 @@
-import segmentation.network as seg
-seg.process_images("/datasets/Market-1501/Market-1501-v15.09.15/bounding_box_test/","/datasets/Market-1501Segmented/test/",1)
--- utils/matutils.py
+++ utils/matutils.py
@@ -1,12 +1,21 @@
+import os
+import torch
+import scipy.io
+import argparse
+import pandas as pd
+import numpy as np
+
+def fliplr(img):
+    inv_idx = torch.arange(img.size(3) - 1, -1, -1).long()  # N x C x H x W
+    img_flip = img.index_select(3, inv_idx)
+    return img_flip
+
 def extractor(model, dataloader):
-    def fliplr(img):
-        inv_idx = torch.arange(img.size(3) - 1, -1, -1).long()  # N x C x H x W
-        img_flip = img.index_select(3, inv_idx)
-        return img_flip
 
     test_names = []
     test_features = torch.FloatTensor()
 
+    #dataloader provides the entire batch and one image from that batch (likely picked at random until every image is used)
     for batch, sample in enumerate(tqdm(dataloader)):
         names, images = sample['name'], sample['img']
 
@@ -18,3 +27,24 @@ def extractor(model, dataloader):
         test_features = torch.cat((test_features, ff), 0)
 
     return test_names, test_features
+"""
+def calculatemAP(imagelabel, guesslist, ignorelabel=None):
+    count = 1.0
+    totalmAP = 0.0
+    for n in range(1,len(guesslist) + 1):
+        if ignorelabel == guesslist[n-1]:
+            pass
+        elif imagelabel == guesslist[n-1]: 
+            totalmAP += count / float(n)
+            count += 1
+
+    totalmAP /= (count - 1)
+    return totalmAP
+
+    import os
+import torch
+import scipy.io
+import argparse
+import pandas as pd
+import numpy as np
+"""
