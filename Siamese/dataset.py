94994a3c8f822e08bbd0f4a84bd5a014d1d58eca
ankit61 <ankit61@tamu.edu>
2018-11-30 01:29:10 -0600

    adding diff transforms

--- Siamese/dataset.py
+++ Siamese/dataset.py
@@ -11,99 +11,94 @@ from itertools import combinations, product
 from functools import reduce
 from random import shuffle, sample
 
-from pytorch_segmentation_detection.transforms import (
-    ComposeJoint,
-    RandomHorizontalFlipJoint
-)
-
 def random_combination(iterable, r):
-    "Random selection from itertools.combinations(iterable, r)"
-    pool = tuple(iterable)
-    n = len(pool)
-    indices = sorted(sample(range(n), r))
-    return [pool[i] for i in indices]
+	"Random selection from itertools.combinations(iterable, r)"
+	pool = tuple(iterable)
+	n = len(pool)
+	indices = sorted(sample(range(n), r))
+	return [pool[i] for i in indices]
 
 class SiameseSampler(torch.utils.data.Sampler):
-    def __init__(self, data_source):
-        self.classes = data_source.getList()
-        # construct the keys
-        self.total = []
-        diff_pairs = random_combination(combinations(self.classes, 2), 2250)
-        same_pairs = sample([(c, c) for c in self.classes], 225)
+	def __init__(self, data_source):
+		self.classes = data_source.getList()
+		# construct the keys
+		self.total = []
+		diff_pairs = random_combination(combinations(self.classes, 2), 2250)
+		same_pairs = sample([(c, c) for c in self.classes], 225)
 
-        #print(len(diff_pairs))
-        #print(len(same_pairs))
-        pairs = diff_pairs + same_pairs
-        
-        for pair in pairs:
-            rng1 = range(0, data_source.getClassLength(pair[0]))
-            rng2 = range(0, data_source.getClassLength(pair[1]))
-            rngboth = tuple(product(rng1, rng2)) 
-            indices = random_combination(rngboth, 5)
+		#print(len(diff_pairs))
+		#print(len(same_pairs))
+		pairs = diff_pairs + same_pairs
+		
+		for pair in pairs:
+			rng1 = range(0, data_source.getClassLength(pair[0]))
+			rng2 = range(0, data_source.getClassLength(pair[1]))
+			rngboth = tuple(product(rng1, rng2)) 
+			indices = random_combination(rngboth, 5)
 
-            for tup in indices:
-                self.total.append(
-                    (pair[0], pair[1], tup[0], tup[1])
-                )
+			for tup in indices:
+				self.total.append(
+					(pair[0], pair[1], tup[0], tup[1])
+				)
 
-        shuffle(self.total)
+		shuffle(self.total)
 
-    def __len__(self):
-        return len(self.total)
-    def __iter__(self):
-        return iter(self.total)
+	def __len__(self):
+		return len(self.total)
+	def __iter__(self):
+		return iter(self.total)
 
 class SiameseDataset(torch.utils.data.Dataset):
-    def __init__(self, path="/datasets/DukeSegmented/train/", test=False):
-        self.path = path
-        self.imgfilenames = sorted(
-            [filename for _, _, filename in os.walk(path)][0])
-        self.dataclass = {}
-        for name in self.imgfilenames:
-            classname = int(name[0:4])
-            if classname not in self.dataclass:
-                self.dataclass[classname] = [name]
-            else:
-                self.dataclass[classname].append(name)
-    
-    def getClassLength(self, classname):
-        return len(self.dataclass[classname])
-    
-    def getList(self):
-        return [name for name in self.dataclass]
-    
-    def __len__(self):
-        return len(self.dataclass)
-    
-    def __getitem__(self, key):
-        # key structure (class1, class2, index1, index2) #
-        tensor_trans = ComposeJoint(
-            [
-                [transforms.ToTensor(), transforms.ToTensor()]
-            ]
-        )
+	def __init__(self, path, transforms, test=False):
+		self.path = path
+		self.imgfilenames = sorted(
+			[filename for _, _, filename in os.walk(path)][0])
+		self.dataclass = {}
+		for name in self.imgfilenames:
+			classname = int(name[0:4])
+			if classname not in self.dataclass:
+				self.dataclass[classname] = [name]
+			else:
+				self.dataclass[classname].append(name)
 
-        imgname1 = self.dataclass[key[0]][key[2]]
-        imgname2 = self.dataclass[key[1]][key[3]]
+		self.transforms = transforms
+	
+	def getClassLength(self, classname):
+		return len(self.dataclass[classname])
+	
+	def getList(self):
+		return [name for name in self.dataclass]
+	
+	def __len__(self):
+		return len(self.dataclass)
+	
+	def __getitem__(self, key):
+		# key structure (class1, class2, index1, index2) #
 
-        img1 = Image.open(self.path + imgname1)
-        img2 = Image.open(self.path + imgname2)
-        
-        if key[0] == key[1]:
-            same = 1
-        else:
-            same = 0
+		imgname1 = self.dataclass[key[0]][key[2]]
+		imgname2 = self.dataclass[key[1]][key[3]]
 
-        imgtensor1, imgtensor2 = tensor_trans([img1, img2])
+		img1 = Image.open(self.path + imgname1)
+		img2 = Image.open(self.path + imgname2)
 
-        return imgtensor1, imgtensor2, same
+
+		
+		if key[0] == key[1]:
+			same = 1
+		else:
+			same = 0
+
+		imgtensor1 = self.transforms(img1)
+		imgtensor2 = self.transforms(img2)
+
+		return imgtensor1, imgtensor2, same
 
 #temporary tests
 #s = SiameseDataset("/datasets/DukeSegmented/train/")
 
 """
 print(
-    s[1495, 1492, 0, 0]
+	s[1495, 1492, 0, 0]
 )
 """
 
