58e8c445d6757cf602878183320d45dffbb0bff8
Benton Guess <bguess10@tamu.edu>
2018-11-30 21:58:04 -0600

    Mat file ready for testing

--- utils/makematfile.py
+++ utils/makematfile.py
@@ -22,14 +22,14 @@ def extractor(_model, _simple_dataloader):
 	test_features = torch.FloatTensor()
 
 	#dataloader provides the entire batch and one image from that batch (likely picked at random until every image is used)
-	for image, name in enumerate(_simple_dataloader):
+	for images, names in enumerate(_simple_dataloader):
 
 		#get a name and an image
 		#names, images = sample['name'], sample['img']
 
 		#run that image through the network for the 
-		ff = model(Variable(images.cuda(), volatile=True))[0].data.cpu()
-		ff = ff + model(Variable(fliplr(images).cuda(), volatile=True))[0].data.cpu()
+		ff = model.forwardforone(Variable(images.cuda(), volatile=True))[0].data.cpu()
+		ff = ff + model.forwardforone(Variable(fliplr(images).cuda(), volatile=True))[0].data.cpu()
 		
 		#normalize the 
 		ff = ff.div(torch.norm(ff, p=2, dim=1, keepdim=True).expand_as(ff))
@@ -39,21 +39,36 @@ def extractor(_model, _simple_dataloader):
 
 	return test_names, test_features
 
-
-wang_folders = [
-	[
-		"/datasets/TAMUvalSegmented/query/",
-		"/datasets/TAMUvalSegmented/gallery/"
-	],
-	[
-		"/datasets/TAMUtestSegmented/query/",
-		"/datasets/TAMUtestSegmented/gallery/"
+if __name__=="__main__":
+	model_path = "/home/ryan/csce-625-person-re-identification/Siamese/trained_resnets/bestMAP.tar"
+	wang_folders = [
+		[
+			"/datasets/TAMUvalSegmented/query/", "/datasets/TAMUvalSegmented/gallery/"
+		],
+		[
+			"/datasets/TAMUtestSegmented/query/", "/datasets/TAMUtestSegmented/gallery/"
+		]
 	]
-]
 
-for dataset in wang_folders:
-    for subset in dataset:
+	checkpoint = torch.load(model_path)
+	model = Siamese()
+	model.cuda()
+	model.load(checkpoint['state_dict'])
+	imgTransforms = transforms.Compose([
+		transforms.Resize(img_size),
+		transforms.ToTensor(),
+		transforms.Normalize(mean=mean, std=std),
+	])
 
-        test_names, test_features = extractor(model, dataloaders[dataset][subset])
-        results = {'names': test_names, 'features': test_features.numpy()}
-        scipy.io.savemat(os.path.join(log_dir, 'feature_%s_%s.mat' % (dataset, subset)), results)
+	for dataset in wang_folders:
+		for directory in dataset:
+
+			gallery_loader = torch.utils.data.DataLoader(
+				SimpleDataset(path=directory, transforms=imgTransforms),
+				batch_size=64, shuffle=False,
+				num_workers=4, pin_memory=True
+			)
+
+			test_names, test_features = extractor(model, )
+			results = {'names': test_names, 'features': test_features.numpy()}
+			scipy.io.savemat(os.path.join(log_dir, 'feature_%s_%s.mat' % (dataset, subset)), results)
